413d58a0b2ac73bab3b50ef485e93615b4702cee
Alisher Zhubanyshev <alisher.zhubanyshev.atwork@gmail.com>
2022-08-11 19:12:35 +0600

    conf

--- pingtest/settings.py
+++ pingtest/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-l7(3806z1dh_b)0@!dz2x!xp^gm%8wa3!m_7j@ag68m+zl8ox!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
