b69353807d6845711f7487c540f544f35e4e3ed9
Alisher Zhubanyshev <alisher.zhubanyshev.atwork@gmail.com>
2022-08-16 12:38:09 +0600

    upload file example

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("upload_file", views.upload_file, name="upload_file"),
+    path("", views.index, name="index"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,37 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .forms import FileForm
+from .models import TestFile
 
 
 def index(req):
-    return render(req, 'main/index.html')
+    return render(req, "main/index.html")
+
+
+def upload_file(request):
+    if request.method == "POST":
+        form = FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = TestFile(
+                name=str(random.randint(0, 1000)),
+                file=request.FILES["docfile"],
+            )
+            newdoc.save()
+
+            return HttpResponseRedirect("/upload_file")
+    else:
+        form = FileForm()
+
+    documents = TestFile.objects.all()
+
+    return render(
+        request,
+        "main/upload.html",
+        {"documents": documents, "form": form},
+    )
